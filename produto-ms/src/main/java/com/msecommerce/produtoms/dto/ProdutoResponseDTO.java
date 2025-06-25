30b25e793347d2eda33cbf89bdf8f5d7a2d3db84
Wdev <wrlandev0@gmail.com>
2025-06-25 11:49:27 -0300

    refactor(produto-ms): adiciona a serialização

--- produto-ms/src/main/java/com/msecommerce/produtoms/dto/ProdutoResponseDTO.java
+++ produto-ms/src/main/java/com/msecommerce/produtoms/dto/ProdutoResponseDTO.java
@@ -1,12 +1,13 @@
 package com.msecommerce.produtoms.dto;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record ProdutoResponseDTO(
+public record ProdutoResponseDTO (
         UUID id,
         String nome,
         String descricao,
         BigDecimal preco,
         Integer estoque
-) {}
+)implements Serializable {}
